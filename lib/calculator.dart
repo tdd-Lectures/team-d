aed36fe924a4b54d3accbf184b105d11ab32b151
PedroBrosque <ctw00946@criticaltechworks.com>
2022-07-19 10:11:46 +0100

    mob

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,16 +1,14 @@
-
 class Calculator {
-  static int add(String number){
-    if(number.isEmpty){
+  static int add(String number) {
+    if (number.isEmpty) {
       return 0;
     }
     var numberList = number.split(",");
-    switch(numberList.length) {
+    switch (numberList.length) {
       case 1:
         return int.parse(number);
       case 2:
-        return 3;
+        return add(numberList[0]) + add(numberList[1]);
     }
-
   }
 }
--- test/calculatorKata_test.dart
+++ test/calculatorKata_test.dart
@@ -1,30 +1,40 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_intro/calculator.dart';
 
-
 void main() {
-  group("Empty String",() {
-    test("Input empty string return 0", (){
+  group("Empty String", () {
+    test("Input empty string return 0", () {
       var sum = Calculator.add("");
-      expect(sum,0);
+      expect(sum, 0);
     });
   });
-   group("One number input",() {
-     test("Input is 1 return the same number", (){
-       var sum = Calculator.add("1");
-       expect(sum,1);
-     });
-     test("Input is 2 return the same number", (){
-       var sum = Calculator.add("2");
-       expect(sum,2);
-     });
+  group("One number input", () {
+    test("Input is 1 return the same number", () {
+      var sum = Calculator.add("1");
+      expect(sum, 1);
+    });
+    test("Input is 2 return the same number", () {
+      var sum = Calculator.add("2");
+      expect(sum, 2);
+    });
   });
 
   group("Input is two numbers", () {
-    test("Input are 1,2 return the 3", (){
+    test("Input are 1,2 return the 3", () {
       var sum = Calculator.add("1,2");
-      expect(sum,3);
+      expect(sum, 3);
+    });
+    test("Input are 2,2 return the 4", () {
+      var sum = Calculator.add("2,2");
+      expect(sum, 4);
+    });
+    test("Input are ,1 return the 1", () {
+      var sum = Calculator.add(",1");
+      expect(sum, 1);
+    });
+    test("Input are 1, return the 1", () {
+      var sum = Calculator.add("1,");
+      expect(sum, 1);
     });
   });
-
 }
